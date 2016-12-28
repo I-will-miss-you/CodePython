c9d9f40ecb7e22e325decbebba682986b2fd4d27
Code36u4r60 <code36u4r60@hotmail.com>
2016-12-28 04:42:43 +0000

    Introdução à programação com o Python
    Microsoft Virtual Academy
    - Introdução à programação com o Python
    
    Aprender:
    Visto - 01 | Introdução
    Visto - 02 | Exibição de texto

---
+++ Microsoft Virtual Academy/python_02/DisplayingText/DisplayingText/DisplayingText.py
@@ -0,0 +1,29 @@
+print('The capybara is the worldslargest rodent')
+print('the capybara likes to live in groups')
+print("the capybara can swim")
+
+print("It's a beautiful day in the neighborhood")
+
+print("The capybara lives in \nSouth America")
+
+print("Hickory Dickory Dock!\nThe mouse ran \t the clock")
+
+print("""Hickory Dickory Dock!
+The mouse ran upthe clock """)
+
+print('''Hickory Dickory Dock!
+The mouse ran upthe clock ''')
+
+print("Here is a string "+"here is more")
+
+print("Can I just print a \ on the screen?")
+# I am inserting a \\ so the appears correctly in the string
+print("But waht if i want \\news")
+
+print("""
+         There once was a movie star icon
+         who preferred to sleep with the light on.
+         They learned how to code
+         a decide that sure glowed
+         and lit up the night using Python!
+         """)
