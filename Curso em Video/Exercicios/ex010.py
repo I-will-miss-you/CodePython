19c5d6014e2ae6dac4a962543b0d7d7bbccb07ad
code36u4r60 <code36u4r60@hotmail.com>
2017-07-03 17:30:57 +0100

    Estudo em Python

---
+++ Curso em Video/Exercicios/ex010.py
@@ -0,0 +1,7 @@
+# Crie um programa que leia quanto dinheiro um pessoa tem na carteira e mostre quantos Dólares ela pode comprar.
+# Considere: US$1,00 = R$3,27
+
+real = float(input('Quanto dinheiro você tem na carteira? R$'))
+dolar = real / 3.27
+
+print('Com R${:.2f} você pode comprar US${:.2f}'.format(real, dolar))
