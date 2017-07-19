647d0b276fcbde434bc16e467335d9bb78d5f8f4
code36u4r60 <code36u4r60@hotmail.com>
2017-07-19 17:21:59 +0100

    Estudo em Python

--- Curso em Video/Exercicios/ex004.py
+++ Curso em Video/Exercicios/ex004.py
@@ -12,4 +12,3 @@ print('É alfanúmerico? {}.'.format(a.isalnum()))
 print('Está em maiúsculas? {}.'.format(a.isupper()))
 print('Está em minúsculas? {}.'.format(a.islower()))
 print('Está capitalizada? {}.'.format(a.istitle()))
-
