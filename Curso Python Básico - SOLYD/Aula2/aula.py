475bf863e90c5f3d9a05413dbf831e8eef4518f7
code36u4r60 <code36u4r60@hotmail.com>
2017-08-04 03:07:27 +0100

    Estudo em Python

---
+++ Curso Python Básico - SOLYD/Aula2/aula.py
@@ -0,0 +1,55 @@
+
+#a = 1
+#b = 1.2
+#c = 'G'
+
+##########################
+#print('Hello World!\nSegundo print')
+#print('Hello World!\tSegundo print')
+
+##########################
+#nome = 'Gilherme'
+#idade = 27
+#altura = 1.78
+
+#tipo_nome = type(nome)
+#tipo_idade = type(idade)
+#tipo_altura = type(altura)
+
+#print(nome)
+#print(tipo_nome)
+#print(idade)
+#print(tipo_idade)
+#print(altura)
+#print(tipo_altura)
+
+#print(nome, 'tem', idade, 'anos e', altura, 'de altura')
+
+#print(nome + ' tem ' + str(idade) + ' anos e ' + str(altura) + ' de altura')
+
+#frase = nome + ' tem ' + str(idade) + ' anos e ' + str(altura) + ' de altura'
+#print(frase)
+
+##########################
+#nome = input('Escreva seu nome: ')
+#idade = input('Escreva sua idade: ')
+#altura = input('Escreva sua altura: ')
+#print(nome, 'tem', idade, 'anos e', altura, 'de altura')
+
+#print(type(nome), type(idade), type(altura))
+
+
+##########################
+numero1 = 27
+numero2 = 52
+numero3 = 12
+
+resultado = numero1 - numero2 / numero3
+print(resultado)
+resultado = (numero1 - numero2) / numero3
+print(resultado)
+
+resultado = 10 ** 2
+print(resultado)
+resultado = 100 ** (1/2)
+print(resultado)
---
+++ Curso Python Básico - SOLYD/Aula2/exercicio.py
@@ -0,0 +1,18 @@
+'''
+EXERCICIO: Faça um programa que pergunte o nome, cpf,
+endereço, idade, altura e telefone e imprima isso
+em um realatorio organizado.
+'''
+
+nome = input('Nome: ')
+cpf = input('CPF: ')
+endereco = input('Endereço: ')
+idade = input('Idade: ')
+telefone = input('Telefone: ')
+
+print(
+    'Nome:', nome,
+    '\n' 'CPF:', cpf,
+    '\n' 'Endereço:', endereco,
+    '\n' 'Idade:', idade,
+    '\n' 'Telefone:', telefone)
