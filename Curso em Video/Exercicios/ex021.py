d2e82bd381be32dba925a274bfa9395b722cb9bb
code36u4r60 <code36u4r60@hotmail.com>
2017-08-16 04:13:00 +0100

    Estudo em Python

--- Curso em Video/Exercicios/ex021.py
+++ Curso em Video/Exercicios/ex021.py
@@ -1,6 +1,10 @@
 #Faça um programa em Python que abra e reproduza o áudio de um arquivo MP3.
 import pygame
-pygame.init()
-pygame.mixer.music.load('musica')
+
+pygame.mixer.init()
+pygame.mixer.music.load('Amy.mp3')
 pygame.mixer.music.play()
-pygame.event.wait()
+
+while pygame.mixer.music.get_busy():
+    pygame.time.Clock().tick(10)
+
